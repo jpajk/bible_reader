b5fb91f63842475f598f59c89094f50222a802e9
alecto <jacekpajak@yandex.com>
2016-02-12 17:19:12 +0100

    Initial

---
+++ README.md
@@ -0,0 +1,4 @@
+bible_app
+=========
+
+A Symfony project created on February 12, 2016, 4:24 pm.
---
+++ src/EntityBundle/Entity/User.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace EntityBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ *
+ * @ORM\Table(name="user")
+ * @ORM\Entity(repositoryClass="EntityBundle\Repository\UserRepository")
+ */
+class User extends BaseUser
+{    
+    public function __construct()
+    {
+        parent::construct();
+    }
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
