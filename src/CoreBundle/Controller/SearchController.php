402a0a31a6d198165e5fdafaf14db32411508c79
alecto <jacekpajak@yandex.com>
2016-02-18 17:06:45 +0100

    Prototype search page

--- src/CoreBundle/Controller/SearchController.php
+++ src/CoreBundle/Controller/SearchController.php
@@ -5,6 +5,7 @@ namespace CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class SearchController extends Controller
 {
@@ -15,16 +16,29 @@ class SearchController extends Controller
     {
     	return $this->render('CoreBundle:Search:search.html.twig');
     }
+
+    /**
+     * @Route("/search/query") 
+     */
+    public function queryAction(Request $req)
+    {
+        $finder = $this->container->get('fos_elastica.finder.bible');
+        $query = $req->query->get('q');
+
+        $result = $finder->find($query . '*');
+        $result_array = [];
+
+        foreach ($result as $key => &$find) 
+        {
+            $a_result = new \stdClass();
+            $a_result->id = $find->getId();
+            $a_result->content = (property_exists($find, 'content')) ? $find->getContent() : null;
+            $result_array[] = $a_result;            
+        }        
+
+        $res = new Response(json_encode($result_array));
+        $res->headers->set('Content-Type', 'application/json');
+
+        return $res;
+    }
 }
-
-/*
-$verse = $this->getDoctrine()
-                   ->getRepository('EntityBundle:Verse')
-                   ->find(1);
-        
-        //$result = $finder->find('1*');
-
-        dump($verse->getTag());
-
-        return new Response('');
- */
--- src/CoreBundle/Resources/public/js/search.js
+++ src/CoreBundle/Resources/public/js/search.js
@@ -4,11 +4,13 @@ $(function() {
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       limit: 10,
       remote: {
-        url: 'http://localhost:9200/_search?q=%QUERY',
+        url: 'http://bibleapp.jesus/app_dev.php/search/query?q=%QUERY',
         wildcard: '%QUERY',
          filter: function(list) {
             console.log(list);
-            return $.map(list, function(drug) { return { name: drug.name, link: drug.link }; });
+            return $.map(list, function(find) { 
+              return { content: find.content, id: find.id }; 
+            });
         }
       } 
     });
@@ -20,7 +22,7 @@ $(function() {
       displayKey: 'name',
       source: bibleSearch.ttAdapter(),
   templates: {
-    suggestion: Handlebars.compile('<a href="{{link}}"><p>{{name}}</p></a>')
+    suggestion: Handlebars.compile('<span>{{content}}</span>')
   }
 
     }); 
