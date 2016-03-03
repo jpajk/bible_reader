c119ba6c075567b560b7a633c20b1981067456ad
alecto <jacekpajak@yandex.com>
2016-03-03 18:06:09 +0100

    Refine further

--- src/CoreBundle/Resources/public/js/search.js
+++ src/CoreBundle/Resources/public/js/search.js
@@ -1,6 +1,7 @@
 var globalBible;
+var isAppended = false;
 
-$(function() {
+$(function () {
       var bibleSearch = new Bloodhound({
       datumTokenizer: Bloodhound.tokenizers.obj.whitespace("name", "link"),
       queryTokenizer: Bloodhound.tokenizers.whitespace,
@@ -10,38 +11,46 @@ $(function() {
         wildcard: '%QUERY',
          filter: function(list) {
             //console.log(list);
-            return $.map(list, function(find) { 
-              return { content: find.content, 
-                       id: find.id, 
-                       chapter: find.chapter, 
+            return $.map(list, function(find) {
+              return { content: find.content,
+                       id: find.id,
+                       chapter: find.chapter,
                        number: find.number,
                        shortname: find.shortname,
                        link: find.link
-                     }; 
+                     };
             });
         }
-      } 
+      }
     });
 
     bibleSearch.initialize();
 
-    globalBible = bibleSearch;    
+    globalBible = bibleSearch;
 });
 
 $(function() {
-  $('.search-wrap').one('keydown', function(e) {
-      $(this).appendTo('.search-holder')
-             .addClass('nav navbar-nav col-xs-8');
+  $('.search-wrap').on('keydown', function(e) {      
 
-      $(this).find('input').focus();
+      if (isAppended === false && $(this).find('input').val() !== "") {
+        $(this).appendTo('.search-holder')
+               .addClass('nav navbar-nav col-xs-8');
+  
+        $(this).find('input').focus();
+  
+        $('.body-wrap').html('<div id="results-wrap" class="row"></div>')
+                       .attr('style', '');
 
-      $('.body-wrap').html('<div id="results-wrap" class="row"></div>')
-                     .attr('style', '');
+        isAppended = true;
+      }
 
-      populateDOM.populateWithResults($(this).find('input'));
+      if ($(this).find('input').val() !== "") {
+        populateDOM.populateWithResults($(this).find('input'));        
+      }
+
     });
 
-    $('.main-banner').fadeIn(1000).css('top', '150px');
+    $('.main-banner').fadeIn(1000).css('top', '150px');  
 
 });
 
@@ -51,20 +60,20 @@ var populateDOM = {
     var object = this;
     var template = Handlebars.templates.search;
 
-    $.each(json, function(index, result) {      
+    $.each(json, function(index, result) {
       returnDOM += template(result);
     });
 
     return returnDOM;
-  },  
+  },
   appendByJQuery : function(readyElement, nodeName) {
     $(nodeName).append(readyElement);
-  },    
+  },
   populateWithResults : function(el) {
     var object = this;
 
     $(el).on('keyup', function() {
-      $('#results-wrap').html('');      
+      $('#results-wrap').html('');
 
       globalBible.search($(el).val(), object.populateSync, object.populateAsync);
     });
@@ -72,7 +81,7 @@ var populateDOM = {
   populateSync : function(json) {
     return;
   },
-  populateAsync : function(json) {        
+  populateAsync : function(json) {
     var results = populateDOM.handleArray(json);
     $('#results-wrap').html(results);
   }
