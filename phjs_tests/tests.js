0e907f8b1ca64a07793582f6aae31e1891a1054e
alecto <jacekpajak@yandex.com>
2016-03-07 18:51:07 +0100

    Add casperjs tests

---
+++ phjs_tests/tests.js
@@ -0,0 +1,26 @@
+var siteUrl = 'http://bibleapp.jesus';
+
+casper.test.begin('Visit the index page on bibleapp', 8, function suite(test) {
+	casper.start(siteUrl, function(response) {		
+		test.assertEquals(response.status, 200, 'response is 200');
+		test.assertTitle('Bible Reader', 'the page title is as expected');
+		test.assertExists('.menu-right', 'menu on the right appears');
+		test.assertEval(function() {
+			return $('.navbar-right li').toArray().length === 3;
+		}, 'there are three elements in the menu on the right');
+		test.assertExists('.fa-twitter', 'twitter icon displays');
+		test.assertExists('.fa-facebook', 'facebook icon displays');
+		test.assertExists('input#main-search', 'the main input displays correctly');
+
+	});
+
+	casper.then(function() {
+		this.sendKeys('#main-search', 'aaron');
+		test.assertExists('#results-wrap', 'the list displays on search');
+
+	});
+
+	casper.run(function() {
+		test.done();
+	});
+});
